8e90a5cdce147b162ce7d40c7873f6d12a01e6a3
Intengine <studio@intengine.pl>
2019-10-10 18:07:07 +0200

    Created "movement engine"
    Yeah, that sounds serious.

---
+++ Assets/Scripts/Player Scripts/MovementMotor.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementMotor : MonoBehaviour
+{
+    public float gravityMultiplier = 1f;
+    public float lerpTime = 10f;
+
+    [HideInInspector]
+    public CharacterController characterController;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 targetDirection = Vector3.zero;
+    private float fallVelocity = 0f;
+
+    public float distanceToGround = 0.1f;
+
+    private bool isGrounded;
+    private Collider myCollider;
+
+    void Awake()
+    {
+      characterController = GetComponent<CharacterController>();
+      myCollider = GetComponent<Collider>();
+    }
+
+    void Start()
+    {
+      distanceToGround = myCollider.bounds.extents.y;
+    }
+
+    void Update()
+    {
+      isGrounded = OnGroundCheck();
+      moveDirection = Vector3.Lerp(moveDirection, targetDirection, Time.deltaTime * lerpTime);
+      moveDirection.y = fallVelocity;
+
+      characterController.Move(moveDirection * Time.deltaTime);
+
+      if (!isGrounded)
+      {
+        fallVelocity -= 90f * gravityMultiplier * Time.deltaTime;
+      }
+    }
+
+    public bool OnGroundCheck()
+    {
+      RaycastHit hit;
+
+      if (characterController.isGrounded)
+      {
+        return true;
+      }
+
+      if(Physics.Raycast(myCollider.bounds.center, -Vector3.up, out hit, distanceToGround + 0.1f))
+      {
+        return true;
+      }
+      return false;
+    }
+
+    public void Move(Vector3 direction)
+    {
+      targetDirection = direction;
+    }
+
+    public void Stop()
+    {
+      moveDirection = Vector3.zero;
+      targetDirection = Vector3.zero;
+    }
+
+    public void Jump(float jumpPower)
+    {
+      if (isGrounded)
+      {
+        fallVelocity = jumpPower;
+      }
+    }
+}
