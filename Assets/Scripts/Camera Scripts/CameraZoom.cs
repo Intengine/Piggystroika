05eb73ad901bf4eb47f68a7848488ef9c08a08d3
Intengine <studio@intengine.pl>
2019-10-21 17:28:43 +0200

    Creating camera zoom
    As in description.

---
+++ Assets/Scripts/Camera Scripts/CameraZoom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
