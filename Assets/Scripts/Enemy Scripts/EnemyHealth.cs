a2ff041f96b54b006fd92759fad37b482c83c6ce
Intengine <studio@intengine.pl>
2019-10-29 16:54:16 +0100

    Creating damage system
    First for player.

---
+++ Assets/Scripts/Enemy Scripts/EnemyHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float health = 100f;
+
+    public void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+
+        if(health <= 0)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public LayerMask enemyLayer;
+    public float damage = 1f;
+    public float radius = 0.3f;
+
+    private EnemyHealth enemyHealth;
+    private bool collided;
+
+    void Update()
+    {
+        CheckForDamage();
+    }
+
+    void CheckForDamage()
+    {
+
+    }
+}
