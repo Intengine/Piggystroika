03aa8a96f36341002dd93d183d3ade668246e2f0
Intengine <studio@intengine.pl>
2019-10-29 17:34:08 +0100

    Creating player damage
    As in description.

---
+++ Assets/Scripts/Enemy Scripts/EnemyAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public LayerMask playerLayer;
+    public float damage = 1f;
+    public float radius = 0.3f;
+
+    private PlayerHealth playerHealth;
+    private bool collided;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
