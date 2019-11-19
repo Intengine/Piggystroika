ad0031e925e2900571019d64106b00bc83403c4a
Intengine <studio@intengine.pl>
2019-11-19 17:16:33 +0100

    Created player spawn position
    As in description.

---
+++ Assets/Scripts/Game Managers/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [SerializeField]
+    private GameObject[] characters;
+
+    [HideInInspector]
+    public int selectedCharacterIndex;
+
+    void Awake()
+    {
+        MakeSingleton();
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += LevelFinishedLoading;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= LevelFinishedLoading;
+    }
+
+    void MakeSingleton()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void LevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.name != "MainMenu")
+        {
+            Vector3 position = GameObject.FindGameObjectWithTag("SpawnPosition").transform.position;
+            Instantiate(characters[selectedCharacterIndex], position, Quaternion.identity);
+        }
+    }
+}
