addf7511ccbc3cf895a4f9be3e0125a09d5be94a
Intengine <studio@intengine.pl>
2019-11-21 18:17:08 +0100

    Buttons to the other levels are showing up
    Small success.

---
+++ Assets/Scripts/Gameplay Scripts/OtherWorlds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherWorlds : MonoBehaviour
+{
+    public GameObject loadButton;
+
+    void OnTriggerEnter(Collider target)
+    {
+        if(target.tag == "Player")
+        {
+            loadButton.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider target)
+    {
+        if (target.tag == "Player")
+        {
+            loadButton.SetActive(false);
+        }
+    }
+}
