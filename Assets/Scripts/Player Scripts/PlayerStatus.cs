d88acac7b5107948f490b9e373562c5333bddc42
Intengine <studio@intengine.pl>
2019-11-25 17:50:17 +0100

    Items Panel working
    As in description.

---
+++ Assets/Scripts/Player Scripts/PlayerStatus.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public GameObject[] playerSwords;
+    private GameObject itemsPanel;
+
+    void Start()
+    {
+        itemsPanel = GameObject.Find("Items Panel");
+        itemsPanel.SetActive(false);
+
+        GameObject.Find("Items Button").GetComponent<Button>().onClick.AddListener(ActivateItemsPanel);
+    }
+
+    public void ActivateItemsPanel()
+    {
+        if(itemsPanel.activeInHierarchy)
+        {
+            itemsPanel.SetActive(false);
+        }
+        else
+        {
+            itemsPanel.SetActive(true);
+        }
+    }
+}
