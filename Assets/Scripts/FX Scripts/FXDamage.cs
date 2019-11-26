db4af5e8b93705a1cb0a86d893ddcb88d530dce0
Intengine <studio@intengine.pl>
2019-11-26 17:49:26 +0100

    Added damage for the tornado
    As in description.

---
+++ Assets/Scripts/FX Scripts/FXDamage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXDamage : MonoBehaviour
+{
+    public LayerMask enemyLayer;
+    public float damage = 20f;
+    public float radius = 2f;
+
+    private EnemyHealth enemyHealth;
+    private bool collided;
+
+    void Update()
+    {
+        CheckForDamage();
+    }
+
+    void CheckForDamage()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius, enemyLayer);
+
+        foreach(Collider h in hits)
+        {
+            enemyHealth = h.GetComponent<EnemyHealth>();
+
+            if(enemyHealth)
+            {
+                collided = true;
+            }
+        }
+        if(collided)
+        {
+            collided = false;
+            enemyHealth.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
