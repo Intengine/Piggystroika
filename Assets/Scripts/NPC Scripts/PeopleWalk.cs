080e8174ef09a93a1c3aa12ebe5400126deca420
Intengine <studio@intengine.pl>
2019-12-02 17:14:28 +0100

    Started to adding some NPC's
    And their "patrol points".

---
+++ Assets/Scripts/NPC Scripts/PeopleWalk.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleWalk : MonoBehaviour
+{
+    public Transform[] walkPoints;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
